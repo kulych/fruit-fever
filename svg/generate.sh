c332258ff51d5e33a057cb39d1e73692e7ded4ee
TK <kulych@kulych.cz>
2019-02-26 19:05:34 +0100

    Added RailGun (beam) texture

---
+++ svg/generate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+python toline.py apple
+python topng.py apple 43
+python toline.py beam
+python topng.py beam 15
+python toline.py bunny
+python topng.py bunny 178
+python toline.py carrot
+python topng.py carrot 500
+python toline.py lemon
+python topng.py lemon 43
+python toline.py orange
+python topng.py orange 43
+python toline.py pear
+python topng.py pear 43
